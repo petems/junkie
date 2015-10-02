a1795ba436774dd704fe190d63dd0f0ba450c31a
leomilrib <leo@leomilrib.com>
2015-10-02 00:17:34 -0300

    oooops... this stays here

--- app.rb
+++ app.rb
@@ -1,3 +1,23 @@
+require 'sinatra'
+require 'sinatra/flash'
+require 'octokit'
+require 'httparty'
+require 'faraday-http-cache'
+require 'json'
+require './app_helpers'
+require "./app"
+require 'sass/plugin/rack'
+
+stack = Faraday::RackBuilder.new do |builder|
+  builder.use Faraday::HttpCache
+  builder.use Octokit::Response::RaiseError
+  builder.adapter Faraday.default_adapter
+end
+Octokit.middleware = stack
+
+Sass::Plugin.options[:style] = :compressed
+use Sass::Plugin::Rack
+
 enable :sessions
 set :session_secret, (ENV["SESSION_SECRET"] || "this is session secret")
 
--- config.ru
+++ config.ru
@@ -1,23 +1,3 @@
 #\ -s puma
 
-require 'sinatra'
-require 'sinatra/flash'
-require 'octokit'
-require 'httparty'
-require 'faraday-http-cache'
-require 'json'
-require './app_helpers'
-require "./app"
-require 'sass/plugin/rack'
-
-stack = Faraday::RackBuilder.new do |builder|
-  builder.use Faraday::HttpCache
-  builder.use Octokit::Response::RaiseError
-  builder.adapter Faraday.default_adapter
-end
-Octokit.middleware = stack
-
-Sass::Plugin.options[:style] = :compressed
-use Sass::Plugin::Rack
-
 run Sinatra::Application
